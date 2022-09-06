35002696e66c150783172f32ec9fc446fd54eed9
Cristian Tejada <ctejadah97@gmail.com>
2022-09-06 09:42:20 -0500

    netlify

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,7 +1,9 @@
 const API = process.env.API;
 
 const getData = async (id) => {
+  
   const apiURl = id ? `${API}${id}` : API;
+
   try {
     const response = await fetch(apiURl);
     const data = await response.json();
